b1c5a4d4255ed9e205a8bedd7e335db5aae2b111
Lucjan Dudek <lucekdudek@gmail.com>
2022-11-10 10:26:56 +0100

    Update codestyle

--- 02_weather_stats/weatherstatsapi/weather/settings.py
+++ 02_weather_stats/weatherstatsapi/weather/settings.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class AppSettings(BaseSettings):
     redis_host: str = "192.168.0.1"
-    redis_port: int = 6379  
+    redis_port: int = 6379
     redis_password: str = "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81"
     redis_db: int = 0
 
