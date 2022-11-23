db8da0a1848fd53aa8a794982c5f232cc6d2efcc
Lucjan Dudek <lucekdudek@gmail.com>
2022-11-23 15:35:47 +0100

    New api image

--- weather_stats/weatherstatsapi/weather/settings.py
+++ weather_stats/weatherstatsapi/weather/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class AppSettings(BaseSettings):
-    redis_host: str = "192.168.0.2"
+    redis_host: str = ""
     redis_port: int = 6379
     redis_password: str = ""
     redis_db: int = 0
