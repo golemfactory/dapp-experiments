d0a9ac19baa10cd69de0d99ac2a8db916009d72b
Lucjan Dudek <lucekdudek@gmail.com>
2023-01-03 18:38:36 +0100

    Fix sleep

--- apipinger/swarm_pinger.py
+++ apipinger/swarm_pinger.py
@@ -8,10 +8,8 @@ PINGER_URL = "http://localhost:8080/api/v1"
 
 
 async def run_pinger(session, idx):
-    print(f"pinger-{idx}:hi")
     async with session.get(PINGER_URL) as response:
         status_code = response.status
-    print(f"pinger-{idx}:bye")
 
 
 async def get_measurements(session):
@@ -20,21 +18,18 @@ async def get_measurements(session):
 
 
 async def main(*args, **kwargs):
-    print("main:hi")
-    start_at = time.time()
     async with aiohttp.ClientSession() as session:
-        print("main:tasks")
-        tasks = [asyncio.create_task(run_pinger(session, idx)) for idx in range(100)]
+        tasks = []
+        for idx in range(100):
+            tasks.append(asyncio.create_task(run_pinger(session, idx)))
+            await asyncio.sleep(1)
         for task in tasks:
-            print("main:await")
             await task
-        print("main:measurements")
         measurements = await get_measurements(session)
-    print("main:json")
     print(json.dumps(measurements, indent=2))
-    end_at = time.time()
-    print(f"main:bye:{end_at-start_at}")
 
 
 if __name__ == "__main__":
+    start_at = time.time()
     asyncio.run(main())
+    print(f"main:{time.time()-start_at}")
