7a9e5c47fb97d1572d713071f167c288c0afe8fb
Lucjan Dudek <lucekdudek@gmail.com>
2023-01-03 16:02:37 +0100

    Added basic error handling

--- apipinger/pinger/routers.py
+++ apipinger/pinger/routers.py
@@ -1,3 +1,4 @@
+import time
 from uuid import uuid4
 
 from fastapi import APIRouter
@@ -11,7 +12,14 @@ router_v1: APIRouter = APIRouter()
 
 @router_v1.get("")
 async def get_random():
-    ping = await pinger.ping()
+    start_at = time.time()
+    try:
+        ping = await pinger.ping()
+    except Exception as err:
+        ping = {"err":f"{type(err)}: {err}"}
+    finally:
+        end_at = time.time()
+        ping["ping_time"]= end_at - start_at
     ping["ping_id"] = uuid4()
     MEASUREMENTS.append(ping)
     return jsonable_encoder(ping)
