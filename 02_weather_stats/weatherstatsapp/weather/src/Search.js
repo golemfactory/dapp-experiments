f48d0bb69fed686897a1be4514c31440c971f91b
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2023-02-01 23:16:37 +0100

    Initial implementation.

---
+++ 02_weather_stats/weatherstatsapp/weather/src/Search.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react"
+
+const Search = () => {
+    const [searchInput, setSearchInput] = useState("")
+    const [resultList, setResultList] = useState([])
+    const [weather, setWeather] = useState()
+    const [selectedCity, setSelectedCity] = useState("")
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        fetch(`https://nominatim.openstreetmap.org/search?format=json&polygon=1&addressdetails=1&q=${searchInput}`)
+            .then((result) => result.json())
+            .then((parsedResult) => {
+                setResultList(parsedResult)
+            })
+    }
+
+    const handleResultClick = (clickedData) => {
+        fetch(`/api/v1/weather?lat=${clickedData.lat}&long=${clickedData.lon}`)
+            .then((result) => result.json())
+            .then((parsedResult) => {
+                setWeather(parsedResult)
+            })
+    }
+
+    return (
+        <div className="w-full">
+            <div className="flex items-center justify-center gap-4">
+                <img src="weather.png" alt="logo" className="w-16 h-16 rounded-full" />
+                <h1 className="font-bold text-2xl text-blue-600">
+                    weather <span className="font-thin">app</span>
+                </h1>
+            </div>
+            <div className="mt-10 border-t border-b md:mx-24 py-6">
+                <div className="flex justify-center mx-16 text-gray-400">
+                    <p>
+                        This apps purpose is This apps purpose is This apps purpose is This apps purpose is This apps purpose is This apps
+                        purpose is{" "}
+                    </p>
+                </div>
+            </div>
+            <div className="mt-8 mx-18">
+                <form onSubmit={handleSearch} className="mt-1 flex rounded-md  rounded-r-full rounded-l-full shadow-lg">
+                    <input
+                        type="text"
+                        name="text"
+                        required
+                        value={searchInput}
+                        onChange={(e) => setSearchInput(e.target.value)}
+                        placeholder="Your location"
+                        className="px-6 text-sm py-2 block w-full rounded-l-full border focus:outline-none focus:ring-0 text-black placeholder:text-gray-500"
+                    />
+
+                    <button
+                        type="submit"
+                        className="relative  inline-flex items-center space-x-2 rounded-r-full text-white  text-lg bg-blue-500 px-8 md:px-16 py-2 "
+                    >
+                        Search
+                    </button>
+                </form>
+            </div>
+
+            <div className="mt-14">
+                <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 -mx-4">
+                    {resultList.map((result, index) => (
+                        <React.Fragment key={index}>
+                            {result.class === "place" && result.address && result.address.city && (
+                                <li className=" px-4 mb-4">
+                                    <p
+                                        className={` hover:bg-blue-600 ${
+                                            index == selectedCity ? "bg-blue-600 text-white" : "bg-blue-50 text-gray-500"
+                                        } hover:text-white  font-semibold h-full text-center flex items-center justify-center px-2 py-1 rounded-full text-sm cursor-pointer`}
+                                        onClick={() => {
+                                            handleResultClick({ lat: result.lat, lon: result.lon })
+                                            setSelectedCity(index)
+                                        }}
+                                    >
+                                        {result.address.city}
+                                    </p>
+                                </li>
+                            )}
+
+                            {result.class === "boundary" &&
+                                Object.values(result.address)
+                                    .filter((obj) => obj.toLowerCase().includes(searchInput.toLowerCase()))
+                                    .map((displayobj) => (
+                                        <li className="px-4 mb-4">
+                                            <p
+                                                className={` hover:bg-blue-600 ${
+                                                    index == selectedCity ? "bg-blue-600 text-white" : "bg-blue-50 text-gray-500"
+                                                } hover:text-white  font-semibold h-full text-center flex items-center justify-center px-2 py-1 rounded-full text-sm cursor-pointer`}
+                                                onClick={() => {
+                                                    handleResultClick({ lat: result.lat, lon: result.lon })
+                                                    setSelectedCity(index)
+                                                }}
+                                            >
+                                                {displayobj} {result.address.country}
+                                            </p>
+                                        </li>
+                                    ))}
+                        </React.Fragment>
+                    ))}
+                </ul>
+
+                {weather && (
+                    <div className="flex items-center justify-center w-full mt-16">
+                        <div className="p-10 md:w-2/3 bg-white rounded-full shadow-lg border">
+                            <div className="grid justify-items-center">
+                                <p className="-mb-1 grid justify-items-start text-left font-light text-gray-500">Temp</p>
+                                <p className="text-6xl font-light text-gray-600">{weather}℃</p>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Search
