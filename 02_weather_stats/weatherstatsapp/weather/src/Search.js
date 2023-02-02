d55498453f2c0d0b7140b9931c4ab7a4c375efd1
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2023-02-02 09:17:55 +0100

    Add some loading indicator

--- 02_weather_stats/weatherstatsapp/weather/src/Search.js
+++ 02_weather_stats/weatherstatsapp/weather/src/Search.js
@@ -5,22 +5,30 @@ const Search = () => {
     const [resultList, setResultList] = useState([])
     const [weather, setWeather] = useState()
     const [selectedCity, setSelectedCity] = useState("")
+    const [loading, setLoading] = useState(false)
 
-    const handleSearch = (e) => {
+    const handleSearch = async (e) => {
         e.preventDefault()
+        setLoading(true)
+        await new Promise((resolve) => setTimeout(resolve, 800))
+
         fetch(`https://nominatim.openstreetmap.org/search?format=json&polygon=1&addressdetails=1&q=${searchInput}`)
             .then((result) => result.json())
             .then((parsedResult) => {
                 setResultList(parsedResult)
             })
+        setLoading(false)
     }
 
-    const handleResultClick = (clickedData) => {
+    const handleResultClick = async (clickedData) => {
+        setLoading(true)
+        await new Promise((resolve) => setTimeout(resolve, 600))
         fetch(`/api/v1/weather?lat=${clickedData.lat}&long=${clickedData.lon}`)
             .then((result) => result.json())
             .then((parsedResult) => {
                 setWeather(parsedResult)
             })
+        setLoading(false)
     }
 
     return (
@@ -46,17 +54,36 @@ const Search = () => {
                         name="text"
                         required
                         value={searchInput}
-                        onChange={(e) => setSearchInput(e.target.value)}
+                        onChange={(e) => {
+                            setSearchInput(e.target.value)
+                        }}
                         placeholder="Your location"
                         className="px-6 text-sm py-2 block w-full rounded-l-full border focus:outline-none focus:ring-0 text-black placeholder:text-gray-500"
                     />
-
-                    <button
-                        type="submit"
-                        className="relative  inline-flex items-center space-x-2 rounded-r-full text-white  text-lg bg-blue-500 px-8 md:px-16 py-2 "
-                    >
-                        Search
-                    </button>
+                    <div className="w-1/4">
+                        <button
+                            type="submit"
+                            className="relative w-full inline-flex items-center space-x-2 rounded-r-full text-white  text-lg bg-blue-500 px-8 md:px-16 py-2 "
+                        >
+                            {loading ? (
+                                <svg
+                                    className="w-7 h-7 mx-4 ml inline-block text-white animate-spin"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                    <path
+                                        className="opacity-75"
+                                        fill="currentColor"
+                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                    ></path>
+                                </svg>
+                            ) : (
+                                "Search"
+                            )}
+                        </button>
+                    </div>
                 </form>
             </div>
 
