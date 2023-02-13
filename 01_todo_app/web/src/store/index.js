948f8603c3c89d01a1ee8626ce1746cce78fe36d
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2023-02-13 17:03:51 +0100

    Prettier on all files

--- 01_todo_app/web/src/store/index.js
+++ 01_todo_app/web/src/store/index.js
@@ -1,84 +1,85 @@
-import {configureStore, createSlice, createAsyncThunk} from '@reduxjs/toolkit'
-import request, {DELETE, GET, POST} from "./request";
+import {
+  configureStore,
+  createSlice,
+  createAsyncThunk,
+} from "@reduxjs/toolkit";
+import request, { DELETE, GET, POST } from "./request";
 import _ from "lodash";
-import {get} from './localstorage';
+import { get } from "./localstorage";
 
 export const getMessages = createAsyncThunk(
-  'messages/fetch',
+  "messages/fetch",
   async (act, thunk) => {
     try {
       const response = await request(GET, "/api/list");
-      thunk.dispatch(updateMessages(response.data))
+      thunk.dispatch(updateMessages(response.data));
     } catch (e) {
-      console.error(e)
-      return thunk.rejectWithValue(_.get(e, "response.data", "Unknown"))
+      console.error(e);
+      return thunk.rejectWithValue(_.get(e, "response.data", "Unknown"));
     }
   }
-)
+);
 
 export const addMessage = createAsyncThunk(
-  'messages/add',
+  "messages/add",
   async (act, thunk) => {
     try {
       const response = await request(POST, `/api/add`, act);
-      thunk.dispatch(getMessages())
+      thunk.dispatch(getMessages());
     } catch (e) {
-      return thunk.rejectWithValue(_.get(e, "response.data", "Unknown"))
+      return thunk.rejectWithValue(_.get(e, "response.data", "Unknown"));
     }
   }
-)
+);
 
 export const deleteMessage = createAsyncThunk(
-  'messages/delete',
+  "messages/delete",
   async (act, thunk) => {
     try {
       const response = await request(POST, `/api/${act}/delete`);
-      thunk.dispatch(getMessages())
+      thunk.dispatch(getMessages());
     } catch (e) {
-      return thunk.rejectWithValue(_.get(e, "response.data", "Unknown"))
+      return thunk.rejectWithValue(_.get(e, "response.data", "Unknown"));
     }
   }
-)
+);
 
 export const updateMessage = createAsyncThunk(
-  'messages/update',
+  "messages/update",
   async (act, thunk) => {
     try {
       const response = await request(POST, `/api/${act}/update`);
-      thunk.dispatch(getMessages())
+      thunk.dispatch(getMessages());
     } catch (e) {
-      return thunk.rejectWithValue(_.get(e, "response.data", "Unknown"))
+      return thunk.rejectWithValue(_.get(e, "response.data", "Unknown"));
     }
   }
-)
-
+);
 
 export const appSlice = createSlice({
-  name: 'app',
+  name: "app",
   initialState: {
     messages: [],
     nav: {
       modal: null,
       error: null,
-    }
+    },
   },
   reducers: {
     updateMessages: (state, action) => {
-      state.messages = action.payload
+      state.messages = action.payload;
     },
     updateNav: (state, action) => {
-      state.nav = _.merge(state.nav, action.payload)
+      state.nav = _.merge(state.nav, action.payload);
     },
   },
-  extraReducers: (builder) => {
-  },
-})
+  extraReducers: (builder) => {},
+});
 
-export const {updateMessages, updateNav} = appSlice.actions;
-
+export const { updateMessages, updateNav } = appSlice.actions;
 
 export default configureStore({
   reducer: {
     app: appSlice.reducer,
-  }
-})
+  },
+});
